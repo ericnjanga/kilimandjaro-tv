3a72a1c4897c21912f94f071b067dc72cd50bd8b
Pet Projects <petprojects@Mungo-Digital-Imac1.local>
2018-10-30 23:04:07 -0400

    adding preloader placeholder

--- src/components/ModalVideo.js
+++ src/components/ModalVideo.js
@@ -3,10 +3,27 @@ import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';  
 import Slide from '@material-ui/core/Slide';
 import VimeoPlayer from '@vimeo/player';
+import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 function Transition(props) {
   return <Slide direction="up" {...props} />;
 }
+const styles = theme => ({
+  progress: {
+    margin: theme.spacing.unit * 2,
+    width: '40px',
+    height: '40px',
+    position: 'absolute',
+    left: '50%',
+    marginLeft: '-20px',
+    zIndex: '1',
+  },
+  iFrame: {
+    position: 'relative',
+    zIndex: '2',
+  }
+});
 
 class ModalVideo extends React.Component {
 
@@ -46,7 +63,7 @@ class ModalVideo extends React.Component {
 
   render() {
 
-    const { active, videoId } = this.props;
+    const { active, videoId, classes } = this.props;
 
     return ( 
       <Dialog
@@ -59,9 +76,10 @@ class ModalVideo extends React.Component {
         style={{margin:0}}
       >
         <DialogContent style={{ width:'600px' }}>
+        <CircularProgress className={classes.progress} />
           <iframe
             src={`https://player.vimeo.com/video/${videoId}`}
-            className="kilimansjaro-iframe"
+            className={`kilimansjaro-iframe ${classes.iFrame}`}
             width="100%"
             height="320"
             frameBorder="0"
@@ -75,4 +93,5 @@ class ModalVideo extends React.Component {
   }
 }
 
-export default ModalVideo;
+
+export default withStyles(styles)(ModalVideo);
